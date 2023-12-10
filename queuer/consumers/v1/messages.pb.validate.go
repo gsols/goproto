2e888e9f12aa10252c97415eaa57289a1a7c2517
nickaguilarh <nickaguilarh@users.noreply.github.com>
2023-12-10 04:25:35 +0000

    feat: remove unnecesary body [gsols/proto-apis@90a5a232a2a017c7a4bfbd7c09cd116c023717ae]

--- queuer/consumers/v1/messages.pb.validate.go
+++ queuer/consumers/v1/messages.pb.validate.go
@@ -35,9 +35,6 @@ var (
 	_ = sort.Sort
 )
 
-// define the regex for a UUID once up-front
-var _messages_uuidPattern = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
-
 // Validate checks the field values on PublishConsumerStatsRequest with the
 // rules defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -324,18 +321,6 @@ func (m *RegisterConsumerInfoRequest) validate(all bool) error {
 
 	var errors []error
 
-	if err := m._validateUuid(m.GetConsumerId()); err != nil {
-		err = RegisterConsumerInfoRequestValidationError{
-			field:  "ConsumerId",
-			reason: "value must be a valid UUID",
-			cause:  err,
-		}
-		if !all {
-			return err
-		}
-		errors = append(errors, err)
-	}
-
 	if all {
 		switch v := interface{}(m.GetConsumerInfo()).(type) {
 		case interface{ ValidateAll() error }:
@@ -367,14 +352,6 @@ func (m *RegisterConsumerInfoRequest) validate(all bool) error {
 
 	if len(errors) > 0 {
 		return RegisterConsumerInfoRequestMultiError(errors)
-	}
-
-	return nil
-}
-
-func (m *RegisterConsumerInfoRequest) _validateUuid(uuid string) error {
-	if matched := _messages_uuidPattern.MatchString(uuid); !matched {
-		return errors.New("invalid uuid format")
 	}
 
 	return nil
