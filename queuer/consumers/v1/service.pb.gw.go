706a3f5059b7560946c5b1c1f35d9f9c9b2b9d1b
nickaguilarh <7452380+nickaguilarh@users.noreply.github.com>
2024-05-08 15:11:25 +0000

    feat: add presence channel and unauthorize consumer [gsols/proto-apis@6ff34c0659ea238318c759b327b83d323db97a8a]

--- queuer/consumers/v1/service.pb.gw.go
+++ queuer/consumers/v1/service.pb.gw.go
@@ -31,6 +31,32 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+func request_PresenceService_IAmAlive_0(ctx context.Context, marshaler runtime.Marshaler, client PresenceServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IAmAliveRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.IAmAlive(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PresenceService_IAmAlive_0(ctx context.Context, marshaler runtime.Marshaler, server PresenceServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IAmAliveRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.IAmAlive(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_ConsumerService_RegisterConsumer_0(ctx context.Context, marshaler runtime.Marshaler, client ConsumerServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq RegisterConsumerRequest
 	var metadata runtime.ServerMetadata
@@ -465,6 +491,40 @@ func local_request_ConsumerService_GetSubscribedStreams_0(ctx context.Context, m
 
 }
 
+// RegisterPresenceServiceHandlerServer registers the http handlers for service PresenceService to "mux".
+// UnaryRPC     :call PresenceServiceServer directly.
+// StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
+// Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterPresenceServiceHandlerFromEndpoint instead.
+func RegisterPresenceServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server PresenceServiceServer) error {
+
+	mux.Handle("POST", pattern_PresenceService_IAmAlive_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/queuer.consumers.v1.PresenceService/IAmAlive", runtime.WithHTTPPathPattern("/v1/presence/live"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PresenceService_IAmAlive_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PresenceService_IAmAlive_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	return nil
+}
+
 // RegisterConsumerServiceHandlerServer registers the http handlers for service ConsumerService to "mux".
 // UnaryRPC     :call ConsumerServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -705,6 +765,77 @@ func RegisterConsumerServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 
 	return nil
 }
+
+// RegisterPresenceServiceHandlerFromEndpoint is same as RegisterPresenceServiceHandler but
+// automatically dials to "endpoint" and closes the connection when "ctx" gets done.
+func RegisterPresenceServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+			return
+		}
+		go func() {
+			<-ctx.Done()
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+		}()
+	}()
+
+	return RegisterPresenceServiceHandler(ctx, mux, conn)
+}
+
+// RegisterPresenceServiceHandler registers the http handlers for service PresenceService to "mux".
+// The handlers forward requests to the grpc endpoint over "conn".
+func RegisterPresenceServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return RegisterPresenceServiceHandlerClient(ctx, mux, NewPresenceServiceClient(conn))
+}
+
+// RegisterPresenceServiceHandlerClient registers the http handlers for service PresenceService
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "PresenceServiceClient".
+// Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "PresenceServiceClient"
+// doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
+// "PresenceServiceClient" to call the correct interceptors.
+func RegisterPresenceServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client PresenceServiceClient) error {
+
+	mux.Handle("POST", pattern_PresenceService_IAmAlive_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/queuer.consumers.v1.PresenceService/IAmAlive", runtime.WithHTTPPathPattern("/v1/presence/live"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PresenceService_IAmAlive_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PresenceService_IAmAlive_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	return nil
+}
+
+var (
+	pattern_PresenceService_IAmAlive_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "presence", "live"}, ""))
+)
+
+var (
+	forward_PresenceService_IAmAlive_0 = runtime.ForwardResponseMessage
+)
 
 // RegisterConsumerServiceHandlerFromEndpoint is same as RegisterConsumerServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
